4d8c218e521f0ef1bbe11ba020b2295a80e550de
SANGEETHSANTHOSH SA <167735704+sang2345@users.noreply.github.com>
2024-12-23 21:32:56 +0530

    Add files via upload

---
+++ index (2).php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="New folder/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Stud-Tracker</title>
+    <style type="text/css">
+	body
+	{
+		margin:0px;
+	}
+	</style>
+  </head>
+  <body>
+  <div class="container">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+  <img src="New folder/logoaj.png">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent" style="float:right;">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+          <img src="New folder/slide-4.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="New folder/banner-4.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="New folder/slide-6.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="New folder/banner-3.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+  </div>
+  <p></p>
+  <h4>ACADEMICS</h4>
+  <p>A.J. College of Science and Technology was established in 1995 on the side of national high way near Kumaran Asan Memorial. It was the fulfillment of a long cherished desire of the management to start an institution of higher education offering degree and post graduate courses in professional area. In the beginning B.Sc Computer Science, B.Sc Electronics and B.Com with Computer Application were introduced by taking into consideration through great demand for professionals in the field of Information Technology.</p>
+  
+  <div class="card-deck">
+  <div class="card">
+    <img src="template/img/college_1.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">About Us</h5>
+      <p class="card-text">A.J.College of Science and Technology was established in 1995 at Thonnakkal. It is the fulfilment of a cherished desire of Dr.M.A.Abdul Jabbar</p>
+    </div>
+    
+  </div>
+  <div class="card">
+
+    <img src="template/img/college-2.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Our Vission</h5>
+      <p class="card-text">To be an academic centre of excellence in quality, research, teaching, learning and education.</p>
+    </div>
+   
+  </div>
+  <div class="card">
+    <img src="template/img/college-3.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Our Mission</h5>
+      <p class="card-text">To create a learning environment that enables students to be leaders with commitment towards the society.</p>
+    </div>
+    
+  </div>
+</div>
+  <p></p>
+  <h4>LOCATE US</h4>
+  </div>
+ <div class="container-fluid">
+ <div class="col-sm-12">
+ <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7889.160609008356!2d76.847489!3d8.636227!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x99828d98dc3d9766!2sA.J%20College%20of%20Science%20%26%20Technology!5e0!3m2!1sen!2sin!4v1663756530370!5m2!1sen!2sin" height="450" style="border:0; width:100%;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+ </div>
+ </div>
+ <div class="col-sm-12" style="background-color:#192f59; color:#FFFFFF; text-align:center; padding:10px;">Copyright © Stud-Tracker. All rights reserved.</div>
+ </div>
+    <script src="New folder/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="New folder/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="New folder/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
